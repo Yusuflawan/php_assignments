dd01c88c9fbe97c93c5c06a99927a3fd1fa300bb
Yusuflawan <75075064+Yusuflawan@users.noreply.github.com>
2021-12-06 03:26:11 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,39 +5,44 @@
 // }
 // myfunction();
 
-// This function returns the number of alphabets present
-function getLength($text){
-	echo strlen($text);
+// Word length function
+function getLength(){
+	$txt = 'yhvhvgg';
+	echo strlen($txt);
 }
+getLength();
 
 
-// This function returns the number of words present in a semtence
+// Word count function
 function wordsCount($sentence){
 	echo str_word_count($sentence);
 }
+wordsCount("i am yusuf lawan umar");
 
-// This function return a word in a reverse order
+
+// Word reverse function
 function getReverse($text){
 	echo strrev($text);
 }
+getReverse("yusuf");
 
+// function to get the position of a letter in word
+ function getPosition($name, $letter){
+ 	echo strpos($name, $letter);
+ }
+ getPosition("my name", "n");
 
-// function getPosition($letter, $name){
-// 	echo strpos($name, $letter);
-// }
-// getPosition("y");
-
-
-// function replacing(){
-// 	$text = "I am a student";
-// 	echo str_replace($text, );
-// }
-// replacing($word, $newWord);
+// Replacing function
+ function replacing($oldWord, $newWord){
+ 	$text = "I am a student";
+ 	echo str_replace($oldWord, $newWord, $text);
+ }
+ replacing("student", "teacher");
 
 // function to checkwheather an input is an integer or not
 function check($input){
 	var_dump(is_int($input));
 }
-
+check("yusuf");
 
 ?>
